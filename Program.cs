1569317482a5275cdffbd6141073e53b282c5351
Jull <jullweas@gmail.com>
2022-10-04 18:49:33 +0300

    Задача 2(4)

--- Program.cs
+++ Program.cs
@@ -1,15 +1,39 @@
-﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-int a, b;
+int a, b, c;
+Console.Write("Введите первое число для сравнения: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write("Введите второе число для сравнения: ");
+int.TryParse(Console.ReadLine()!, out b);
+Console.Write("Введите третье число для сравнения: ");
+int.TryParse(Console.ReadLine()!, out c);
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"Наибольшее число = {max}");
+
+
+/* Другой вариант
+int a, b, c;
 Console.Write("Введите первое число для сравнения: ");
 a = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число для сравнения: ");
 b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число для сравнения: ");
+c = int.Parse(Console.ReadLine()!);
 if (a > b)
 {
-    Console.WriteLine($"Наибольшее число = {a}");
+    if (a > c)
+        Console.WriteLine($"Наибольшее число = {a}");
+    else
+        Console.WriteLine($"Наибольшее число = {c}");
+
 }
 else
 {
-    Console.WriteLine($"Наибольшее число = {b}");
+    if (b > c)
+        Console.WriteLine($"Наибольшее число = {b}");
+    else
+        Console.WriteLine($"Наибольшее число = {c}");
 }
+*/
